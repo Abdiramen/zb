8dd7f0f2ac3f21ee3185a5d3f3c6adb9588dbbcd
Abdirahman Osman <abdirahman.osmanthus@gmail.com>
2026-07-04 17:40:27 -0500

    Un-C your loops

--- internal/backend/builtin.go
+++ internal/backend/builtin.go
@@ -67,8 +67,7 @@ func fetchURL(ctx context.Context, drv *zbstore.Derivation, realStoreDir string)
 	}
 
 	var err error
-	for i := 0; i < len(urls); i++ {
-		href = urls[i]
+	for _, href := range urls {
 		err = getURL(ctx, drv, realStoreDir, href)
 		if err == nil {
 			break
